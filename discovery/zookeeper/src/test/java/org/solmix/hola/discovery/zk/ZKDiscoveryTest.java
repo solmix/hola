ebba3e2918a526f0a5af93e1dca0183a67269d5c
solmix <solmix.f@gmail.com>
2014-09-17 14:29:16 -0200

    test

---
+++ discovery/zookeeper/src/test/java/org/solmix/hola/discovery/zk/ZKDiscoveryTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2014 The Solmix Project
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.gnu.org/licenses/ 
+ * or see the FSF site: http://www.fsf.org. 
+ */
+package org.solmix.hola.discovery.zk;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+/**
+ * 
+ * @author solmix.f@gmail.com
+ * @version $Id$  2014年9月17日
+ */
+
+public class ZKDiscoveryTest
+{
+
+    /**
+     * 测试注册
+     */
+    @Test
+    public void testRegister() {
+        fail("Not yet implemented");
+    }
+    /**
+     * 测试取消注册
+     */
+    @Test
+    public void testUnRegister() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testAddListener() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testRemoveListener() {
+        fail("Not yet implemented");
+    }
+    /**
+     * 测试接口为多个
+     */
+    @Test
+    public void testMutiServices() {
+        fail("Not yet implemented");
+    }
+    /**
+     * 测试失败重新发送
+     */
+    @Test
+    public void testFailedback() {
+        fail("Not yet implemented");
+    }
+}
